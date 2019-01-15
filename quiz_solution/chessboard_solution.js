5427b0bda87d00f4377b0e7a0153222ef4e4e6d3
Unknown <spsr5032@gmail.com>
2019-01-16 00:36:34 +0700

    add comment in chessboard_solution.js

--- quiz_solution/chessboard_solution.js
+++ quiz_solution/chessboard_solution.js
@@ -2,17 +2,19 @@ let size = 0,
     result = "";
 
 function chessboard(size) {
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) { 
+            //if the position on chessboard is even row & column or odd row & column print ' '
             if ((j % 2 === 0 && i % 2 === 0) || (j % 2 === 1 && i % 2 === 1)) {
         result += " ";
     } else {
         result += "#"; 
     }
         }
+        //enter new line
         result += "\n";
     }
+    //show the result
     console.log(result);
 }
-
 //credit: https://codereview.stackexchange.com/questions/165205/print-a-chessboard
